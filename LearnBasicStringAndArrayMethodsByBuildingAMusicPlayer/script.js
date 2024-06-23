1abc7c377a1e00ed70356f32b6e46ddabf54a3c0
sergeistetsko <sergeistetsko2021@yandex.by>
2024-06-23 21:03:53 +0300

    add changes

--- LearnBasicStringAndArrayMethodsByBuildingAMusicPlayer/script.js
+++ LearnBasicStringAndArrayMethodsByBuildingAMusicPlayer/script.js
@@ -6,17 +6,26 @@ const nextButton = document.getElementById('next')
 const previousButton = document.getElementById('previous')
 const shuffleButton = document.getElementById('shuffle')
 
-const allSongs = [{
+const allSongs = [
+    {
     id: 0,
     title: "Scratching The Surface",
     artist: "Quincy Larson",
     duration: "4:25",
     src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
-},
-{
+    },
+    {
     id: 1,
     title: "Can't Stay Down",
     artist: "Quincy Larson",
     duration: "4:15",
     src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
-}]
+    },
+    {
+    id: 2,
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
+    }
+]
